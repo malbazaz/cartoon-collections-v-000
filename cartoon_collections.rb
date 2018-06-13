06da8505b13d719802df92f1e5b15f2bc18c9a07
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-13 21:31:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
+  array.select do |taster|
+    taster == include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+end 
